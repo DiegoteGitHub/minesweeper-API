7faa19ce0a0c9fe833b8930f04cd8959e40ed1f1
Israel Diego Lalo <diego.lalo@gmail.com>
2021-03-07 22:35:28 -0300

    Added create method in GameService

--- src/main/java/com/dlalo/minesweeperapi/service/GameService.java
+++ src/main/java/com/dlalo/minesweeperapi/service/GameService.java
@@ -2,7 +2,27 @@ package com.dlalo.minesweeperapi.service;
 
 import org.springframework.stereotype.Service;
 
+import com.dlalo.minesweeperapi.repository.GameRepository;
+
 @Service
 public class GameService {
+	
+	private final GameRepository gameRepository;
+	
+    public GameService(final GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+    
+    public Game create(final CreateRequest aRequest) {
+        final Game newGame = Game.builder()
+            .status("NEW")
+            .username(aRequest.getUsername())
+            .rows(aRequest.getRows())
+            .columns(aRequest.getColumns())
+            .mines(aRequest.getMines())
+            .build();
+        newGame.start();
+        return this.gameRepository.save(newGame);
+    }
 
 }
