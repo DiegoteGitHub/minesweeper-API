b42b6c5cfe252a294579f4f050ac8086a0356f6a
Israel Diego Lalo <diego.lalo@gmail.com>
2021-03-07 22:57:24 -0300

    Added pause, openCell and flagCell methods

--- src/main/java/com/dlalo/minesweeperapi/service/GameService.java
+++ src/main/java/com/dlalo/minesweeperapi/service/GameService.java
@@ -1,30 +1,68 @@
 package com.dlalo.minesweeperapi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.dlalo.minesweeperapi.model.CreateRequest;
+import com.dlalo.minesweeperapi.model.EventRequest;
 import com.dlalo.minesweeperapi.model.Game;
 import com.dlalo.minesweeperapi.repository.GameRepository;
 
 @Service
 public class GameService {
-	
-	private final GameRepository gameRepository;
-	
-    public GameService(final GameRepository gameRepository) {
-        this.gameRepository = gameRepository;
-    }
-    
-    public Game create(final CreateRequest aRequest) {
-        final Game newGame = Game.builder()
-            .status("NEW")
-            .username(aRequest.getUsername())
-            .rows(aRequest.getRows())
-            .columns(aRequest.getColumns())
-            .mines(aRequest.getMines())
-            .build();
-        newGame.start();
-        return this.gameRepository.save(newGame);
-    }
 
+	private final GameRepository gameRepository;
+
+	public GameService(final GameRepository gameRepository) {
+		this.gameRepository = gameRepository;
+	}
+
+	public Game create(final CreateRequest aRequest) {
+		final Game newGame = Game.builder().status("NEW").username(aRequest.getUsername()).rows(aRequest.getRows())
+				.columns(aRequest.getColumns()).mines(aRequest.getMines()).build();
+		newGame.start();
+		return this.gameRepository.save(newGame);
+	}
+
+	public Game findGameById(final String id) {
+		return this.gameRepository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+	}
+
+	public List<Game> findGamesByUsername(final String username) {
+		return this.gameRepository.findByUser(username);
+	}
+
+	public Game pause(final String id) {
+		Game game = findGameById(id);
+		if (game.isGameOver()) {
+			throw new RuntimeException("The Game finished, can not update it.");
+		}
+		game.togglePause();
+		return this.gameRepository.save(game);
+	}
+
+	public Game openCell(final EventRequest request) {
+		Game game = findGameById(request.getGameId());
+		if (game.isPaused()) {
+			throw new RuntimeException("The Game is paused, can not update it.");
+		}
+		if (game.isGameOver()) {
+			throw new RuntimeException("The Game finished, can not update it.");
+		}
+		game.openCell(request.getPosX(), request.getPosY());
+		return this.gameRepository.save(game);
+	}
+
+	public Game flagCell(EventRequest request) {
+		Game game = findGameById(request.getGameId());
+		if (game.isPaused()) {
+			throw new RuntimeException("The Game is paused, can not update it.");
+		}
+		if (game.isGameOver()) {
+			throw new RuntimeException("The Game finished, can not update it.");
+		}
+		game.flagCell(request.getPosX(), request.getPosY());
+		return this.gameRepository.save(game);
+	}
 }
